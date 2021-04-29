45473cd7ef5a88ecdfa4b0749aab233728e1805e
DivyaSrivstava <83064700+DivyaSrivstava@users.noreply.github.com>
2021-04-29 17:42:29 +0530

    Add Functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-Class App extends Component{ 
+Class App = () =>{ 
  render(){
   return (
        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
