6106a31ebc35e33e658cef3f5bfbd9d48871e0ee
DivyaSrivstava <83064700+DivyaSrivstava@users.noreply.github.com>
2021-04-29 17:32:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,11 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react';
+
+Class App extends Component{ 
+ render(){
+  return (
+       <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+      )
+ }
+}
+
 
